a1978bd6c57d3dfcd5ee69e2b723e357aa872efe
Sole02 <ljaeseok02@gmail.com>
2026-03-20 12:08:30 +0900

    STEP 1-4. 반복문을 사용한 계산 및 종료 기능 추가

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -8,37 +8,47 @@ public class Main {
         int num2 = 0;
         int result = 0;
 
-        System.out.println("숫자를 입력하세요.");
-        num = sc.nextInt();
+        while (true) {
+            System.out.println("숫자를 입력하세요.");
+            num = sc.nextInt();
 
-        System.out.println("연산자를 입력하세요.");
-        char op = sc.next().charAt(0);
+            System.out.println("연산자를 입력하세요.");
+            char op = sc.next().charAt(0);
 
-        System.out.println("숫자를 입력하세요.");
-        num2 = sc.nextInt();
-
-        switch (op) {
-            case '+':
-                result = num + num2;
-                break;
-            case '-':
-                result = num - num2;
-                break;
-            case '*':
-                result = num * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("0으로 나눌 수 없습니다.");
-                    return;
-                }
-                result = num / num2;
-                break;
-            default:
+            if (!(op == '+' || op == '-' || op == '*' || op == '/')) {
                 System.out.println("잘못된 연산자입니다, 다시 입력해주세요.");
-                return;
-        }
+                continue;
+            }
 
-        System.out.println(num + " " + op + " " + num2 + " = " + result);
+            System.out.println("숫자를 입력하세요.");
+            num2 = sc.nextInt();
+
+            switch (op) {
+                case '+':
+                    result = num + num2;
+                    break;
+                case '-':
+                    result = num - num2;
+                    break;
+                case '*':
+                    result = num * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        continue;
+                    }
+                    result = num / num2;
+                    break;
+            }
+
+            System.out.println(num + " " + op + " " + num2 + " = " + result);
+            System.out.println("계속 계산 하시겠습니까? (exit 입력 시 종료)");
+
+            String exit = sc.next();
+            if (exit.equals("exit")) {
+                break;
+            }
+        }
     }
 }
