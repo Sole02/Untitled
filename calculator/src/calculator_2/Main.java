935a70f4f7ab87fb3389849001edfad2bb83b5e9
Sole02 <ljaeseok02@gmail.com>
2026-03-30 15:29:21 +0900

    STEP 1-4. 계산기 반복 사용, 계산 후 'exit'로 종료

--- calculator/src/calculator_2/Main.java
+++ calculator/src/calculator_2/Main.java
@@ -7,34 +7,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("숫자를 입력하세요.");
-        int num1 = input.nextInt();
-        System.out.print("연산자를 입력하세요.");
-        char op = input.next().charAt(0);
-        System.out.print("숫자를 입력하세요.");
-        int num2 = input.nextInt();
+        while (true) {
+            System.out.print("숫자를 입력하세요.");
+            int num1 = input.nextInt();
+            System.out.print("연산자를 입력하세요.");
+            char op = input.next().charAt(0);
+            System.out.print("숫자를 입력하세요.");
+            int num2 = input.nextInt();
 
-        int result = 0;
+            int result = 0;
 
-        switch (op) {
-            case '+':
-                result = num1 + num2;
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("나눌 수 없습니다.");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+            }
+            System.out.println("결과: " + result);
+
+            System.out.println("계속하시려면 아무 키를 입력, 종료하시려면 'exit'를 입력하세요.");
+            String exit = input.next();
+            if (exit.equals("exit")) {
+                System.out.println("종료합니다.");
                 break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("나눌 수 없습니다.");
-                    return;
-                }
-                result = num1 / num2;
-                break;
-            default:
+            }
         }
-        System.out.println("결과: " + result);
     }
 }
