5faffd111c994d78d571d056a7c5eb0c40660e59
Sole02 <ljaeseok02@gmail.com>
2026-03-30 12:26:37 +0900

    STEP 1-3. 입력된 수와 연산 기호로 결과값 출력

--- calculator/src/calculator_2/Main.java
+++ calculator/src/calculator_2/Main.java
@@ -9,10 +9,32 @@ public class Main {
 
         System.out.print("숫자를 입력하세요.");
         int num1 = input.nextInt();
-        System.out.println("연산자를 입력하세요.");
+        System.out.print("연산자를 입력하세요.");
         char op = input.next().charAt(0);
         System.out.print("숫자를 입력하세요.");
         int num2 = input.nextInt();
 
+        int result = 0;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눌 수 없습니다.");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+        }
+        System.out.println("결과: " + result);
     }
 }
