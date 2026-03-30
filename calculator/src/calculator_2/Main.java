9f643f0a9377b1611f375e28db22ce0748006665
Sole02 <ljaeseok02@gmail.com>
2026-03-30 10:26:37 +0900

    STEP 1-1. Scanner를 사용하여 정수 입력받기

---
+++ calculator/src/calculator_2/Main.java
@@ -0,0 +1,16 @@
+package calculator_2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("숫자를 입력하세요.");
+        int num1 = input.nextInt();
+        System.out.print("숫자를 입력하세요.");
+        int num2 = input.nextInt();
+
+    }
+}
