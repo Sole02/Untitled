a07e6fc1a1fc06fc7144c8d7895198ddb15cb7c1
Sole02 <ljaeseok02@gmail.com>
2026-03-31 14:37:35 +0900

    STEP 2-1. 사칙연산 수행 후, 결과값 반환 메서드 구현

--- calculator/src/calculator_2/Calculator.java
+++ calculator/src/calculator_2/Calculator.java
@@ -3,5 +3,45 @@ package calculator_2;
 public class Calculator {
 
 
+// 속성
+    int num1;
+    char op;
+    int num2;
+    int result;
 
+// 생성자
+    public Calculator(int num1, char op, int num2) {
+        this.num1 = num1;
+        this.op = op;
+        this.num2 = num2;
+    }
+
+// 기능
+    public int calculator() {
+        switch (this.op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눌 수 없습니다.");
+                    return 0;
+                }
+                result = num1 / num2;
+                break;
+            default:
+        }
+        return result;
+    }
+
+    void result (int result) {
+        this.result = result;
+        System.out.println("결과 : " + result);
+    }
 }
