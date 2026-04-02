c629a65e0d8b3fbcaf9cf877f117725ecf416d06
Sole02 <ljaeseok02@gmail.com>
2026-04-02 10:44:03 +0900

    Fix. 잘못된 연산자 입력 시 예외 처리 및 재입력 로직 추가

--- calculator/src/calculator_2/Main.java
+++ calculator/src/calculator_2/Main.java
@@ -14,12 +14,18 @@ public class Main {
             int num1 = input.nextInt();
             System.out.print("연산자를 입력하세요.");
             char op = input.next().charAt(0);
+
+            if (!(op == '+' || op == '-' || op == '*' || op == '/')) {
+                System.out.println("잘못된 연산자입니다, 다시 입력해주세요.");
+                continue;
+            }
+
             System.out.print("숫자를 입력하세요.");
             int num2 = input.nextInt();
 
             int result = calculator.calculator(num1, op, num2);
+            System.out.println("계산 기록: " + calculator.getResults());
             System.out.println("결과: " + result);
-            System.out.println("기록: " + calculator.getResults());
 
             System.out.println("계속하시려면 아무 키를 입력, 종료하시려면 'exit'를 입력하세요.");
             String exit = input.next();
