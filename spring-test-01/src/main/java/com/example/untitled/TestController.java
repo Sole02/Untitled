bb34934e2847776e9c3cb86f73e31041543cd1b8
Sole02 <ljaeseok02@gmail.com>
2026-04-08 16:46:16 +0900

    feat: Spring Boot 프로젝트 초기 설정 및 기본 컨트롤러 구현

---
+++ spring-test-01/src/main/java/com/example/untitled/TestController.java
@@ -0,0 +1,15 @@
+package com.example.untitled;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("message", "Test, Message");
+        return "test";  // view
+    }
+}
