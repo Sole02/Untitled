c8d84277c094f10b668ceb49a9093bd19c55382c
Sole02 <ljaeseok02@gmail.com>
2026-04-19 17:06:39 +0900

    feat: 일정 생성, POST 엔드포인트 틀 만들기

---
+++ schedule-v2/schedule-v2/src/main/java/com/example/schedule_v2/controller/ScheduleController.java
@@ -0,0 +1,22 @@
+package com.example.schedule_v2.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/schedule")
+public class ScheduleController {
+    // 속성
+
+
+    // 생성자
+
+
+    // 기능
+
+    // 일정 생성
+    @PostMapping
+    public void createSchedule () {
+    }
+}
