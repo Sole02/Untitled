08d88ebb8b57d053f117c9cbebaf3fe4bc4785e0
Sole02 <ljaeseok02@gmail.com>
2026-04-20 14:51:43 +0900

    feat: 일정 생성 컨트롤러에 서비스 연결

--- schedule-v2/schedule-v2/src/main/java/com/example/schedule_v2/controller/ScheduleController.java
+++ schedule-v2/schedule-v2/src/main/java/com/example/schedule_v2/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.example.schedule_v2.controller;
 
 import com.example.schedule_v2.dto.request.ScheduleCreateRequestDto;
 import com.example.schedule_v2.dto.response.ScheduleCreateResponseDto;
+import com.example.schedule_v2.service.ScheduleService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/schedule")
 public class ScheduleController {
     // 속성
-
+    private final ScheduleService scheduleService;
 
     // 생성자
-
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
 
     // 기능
 
     // 일정 생성
     @PostMapping
     public ScheduleCreateResponseDto createSchedule (@RequestBody ScheduleCreateRequestDto requestDto) {
+        return scheduleService.createSchedule(requestDto);
     }
 }
