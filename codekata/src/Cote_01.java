ba34c453bfda492f77847ed3169b562a33a8bf08
Sole02 <ljaeseok02@gmail.com>
2026-05-01 23:56:53 +0900

    코드 테스트 1

---
+++ codekata/src/Cote_01.java
@@ -0,0 +1,19 @@
+public class Cote_01 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Cote_01 cote = new Cote_01();
+        System.out.println(cote.solution(2, 1)); // 결과: 1
+    }
+}
+
+/**
+ * 두 수의 차 구하기
+ * 문제 설명
+ * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+ * 링크 - https://school.programmers.co.kr/learn/courses/30/lessons/120803
+ */
