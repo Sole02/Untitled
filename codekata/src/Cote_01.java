c7a6f27ddc9a74f426dd61ca96b78c364b2e47a0
Sole02 <ljaeseok02@gmail.com>
2026-05-02 00:07:49 +0900

    코딩 테스트 1

--- codekata/src/Cote_01.java
+++ codekata/src/Cote_01.java
@@ -1,3 +1,10 @@
+/**
+ * 두 수의 차 구하기
+ * 문제 설명
+ * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+ * 링크 - https://school.programmers.co.kr/learn/courses/30/lessons/120803
+ */
+
 public class Cote_01 {
     public int solution(int num1, int num2) {
         int answer = 0;
@@ -5,10 +12,3 @@ public class Cote_01 {
         return answer;
     }
 }
-
-/**
- * 두 수의 차 구하기
- * 문제 설명
- * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
- * 링크 - https://school.programmers.co.kr/learn/courses/30/lessons/120803
- */
