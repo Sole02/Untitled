e354b78c9239fc344d6fd69cadecc9d8fb81b041
JaeSeok <166648140+Sole02@users.noreply.github.com>
2026-05-03 17:38:36 +0900

    Delete ecommerce-backoffice/ecommerce-backoffice directory

--- ecommerce-backoffice/ecommerce-backoffice/src/main/java/com/example/ecommerce_backoffice/common/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce_backoffice.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class CustomerNotFoundException extends ServiceException {
-    public CustomerNotFoundException() {
-        super(HttpStatus.NOT_FOUND, "고객을 찾을 수 없습니다.");
-    }
-}
--- ecommerce-backoffice/ecommerce-backoffice/src/main/java/com/example/ecommerce_backoffice/common/exception/OutOfStockException.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerce_backoffice.common.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class OutOfStockException extends ServiceException {
-    public OutOfStockException() {
-        super(HttpStatus.BAD_REQUEST, "재고가 부족합니다.");
-    }
-}
