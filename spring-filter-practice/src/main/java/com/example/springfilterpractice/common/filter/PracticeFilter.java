19a039f49edd7e9631e4298fb6d2c4946d7e1d4b
Sole02 <ljaeseok02@gmail.com>
2026-05-04 12:53:43 +0900

    feat: Filter 동작 순서

---
+++ spring-filter-practice/src/main/java/com/example/springfilterpractice/common/filter/PracticeFilter.java
@@ -0,0 +1,28 @@
+package com.example.springfilterpractice.common.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class PracticeFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // 요청이 들어올 때 실행 되는 부분
+        System.out.println("PracticeFilter 전");
+
+        filterChain.doFilter(request,response);
+
+        // 요청이 나갈 때 실행 되는 부분
+        System.out.println("PracticeFilter 후");
+    }
+}
