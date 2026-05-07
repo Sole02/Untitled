3eb42ea9fc8539b326d5a22d26ced545e498dd66
Sole02 <ljaeseok02@gmail.com>
2026-05-07 10:14:59 +0900

    feat: SecurityConfig 추가, JwtFilter에 SecurityContext 인증 통합

--- spring-filter-practice/src/main/java/com/example/springfilterpractice/common/filter/JwtFilter.java
+++ spring-filter-practice/src/main/java/com/example/springfilterpractice/common/filter/JwtFilter.java
@@ -5,13 +5,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @Component
 @Slf4j
@@ -49,7 +52,11 @@ public class JwtFilter extends OncePerRequestFilter {
 
         // 토큰 정보 확인
         String username = jwtUtil.extractUsername(jwt);
-        request.setAttribute("username", username);
+//        request.setAttribute("username", username);
+        User user = new User(username,"", List.of());
+
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
+
         filterChain.doFilter(request, response);
 
     }
