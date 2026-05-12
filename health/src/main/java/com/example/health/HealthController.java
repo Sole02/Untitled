df3827ce11f2520d23dd9771d9495a43f52c984d
Sole02 <ljaeseok02@gmail.com>
2026-05-12 20:44:35 +0900

    feat: 헬스체크 컨트롤러에 로그 추가

--- health/src/main/java/com/example/health/HealthController.java
+++ health/src/main/java/com/example/health/HealthController.java
@@ -1,15 +1,22 @@
 package com.example.health;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/log")
     public HealthResponse health() {
+        String a = "hello";
+          log.info("포매팅 지원 확인 {}", a);  // INFO 레벨
+//        log.info("헬스체크 요청");           // INFO 레벨
+//        log.debug("상세 디버깅 정보");       // DEBUG 레벨
+//        log.error("에러 발생");            // ERROR 레벨
         return new HealthResponse(
                 "UP",
                 LocalDateTime.now().toString()
