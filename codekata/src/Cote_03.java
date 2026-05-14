d60fb04bba1bfaaa0ffb3564029c938fb9a7107b
Sole02 <ljaeseok02@gmail.com>
2026-05-14 21:38:42 +0900

    코딩 테스트 3

---
+++ codekata/src/Cote_03.java
@@ -0,0 +1,13 @@
+/**
+ * 몫 구하기
+ * 문제 설명
+ * 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120805
+ */
+
+public class Cote_02 {
+    class Solution {
+int solution(int num1, int num2) {
+    int answer = 0;
+    return answer;
+}
