d5dbc8ed4453ff628dc5730c61ff758a8f77686f
JaeSeok <166648140+Sole02@users.noreply.github.com>
2026-05-16 22:55:53 +0900

    코딩 테스트4

---
+++ codekata/src/Cote_04.java
@@ -0,0 +1,14 @@
+/**
+ * 나이 출력
+ * 문제 설명
+ * 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120820
+ */
+
+public class Cote_04 {
+    class Solution {
+        int solution(int age) {
+            int answer = 0;
+            return answer;
+        }
+    }
