72400cd16a169998451f5eb8df24b659cd5ec1fd
JaeSeok <166648140+Sole02@users.noreply.github.com>
2026-05-17 17:06:05 +0900

    코딩 테스트5

---
+++ codekata/src/Cote_05.java
@@ -0,0 +1,15 @@
+/**
+ * 숫자 비교하기
+ * 문제 설명
+ * 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120807
+ */
+
+public class Cote_04 {
+    class Solution {
+        int solution(int num1, int num2) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
