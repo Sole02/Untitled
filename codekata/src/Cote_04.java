b1283767a23601bd163a9a67777a0cd015cabd03
JaeSeok <166648140+Sole02@users.noreply.github.com>
2026-05-30 23:39:24 +0900

    코딩 테스트4

--- codekata/src/Cote_04.java
+++ codekata/src/Cote_04.java
@@ -6,9 +6,10 @@
  */
 
 public class Cote_04 {
-    class Solution {
-        int solution(int age) {
-            int answer = 0;
-            return answer;
-        }
+class Solution {
+    public int solution(int age) {
+        int year = 2022;
+        int answer = year - age + 1;
+        return answer;
     }
+}
