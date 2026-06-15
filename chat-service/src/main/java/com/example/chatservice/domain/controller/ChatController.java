bbd13663af359a60c7d173cbf3b5ba13a3e329d4
Sole02 <ljaeseok02@gmail.com>
2026-06-15 19:23:57 +0900

    refactor: 채팅 메시지 전송 방식을 STOMP 직접 전달에서 Redis Pub/Sub으로 변경

--- chat-service/src/main/java/com/example/chatservice/domain/controller/ChatController.java
+++ chat-service/src/main/java/com/example/chatservice/domain/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.example.chatservice.domain.controller;
 
+import com.example.chatservice.common.config.redis.ChatRedisPublisher;
+import com.example.chatservice.common.config.redis.RedisChatMessage;
 import com.example.chatservice.common.entity.ChatMessage;
 import com.example.chatservice.common.entity.ChatRoom;
 import com.example.chatservice.common.entity.User;
@@ -7,10 +9,8 @@ import com.example.chatservice.common.interceptor.AuthenticatedUser;
 import com.example.chatservice.domain.model.ChatMessageDto;
 import com.example.chatservice.domain.repository.ChatMessageRepository;
 import com.example.chatservice.domain.repository.ChatRoomRepository;
-import com.example.chatservice.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -20,7 +20,7 @@ import java.security.Principal;
 public class ChatController {
 
     private final ChatMessageRepository chatMessageRepository;
-    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatRedisPublisher chatRedisPublisher;
     private final ChatRoomRepository chatRoomRepository;
 
     @MessageMapping("/chat.send")
@@ -34,6 +34,14 @@ public class ChatController {
         ChatMessage message = new ChatMessage(sender, room, dto.getContent());
         chatMessageRepository.save(message);
 
-        messagingTemplate.convertAndSend("/sub/chat/" + room.getId(), dto);
+        RedisChatMessage redisMessage = new RedisChatMessage(
+                message.getChatRoom().getId(),
+                message.getSender().getId(),
+                message.getSender().getName(),
+                message.getContent()
+        );
+
+        chatRedisPublisher.publish(room.getId(), redisMessage);
+
     }
 }
