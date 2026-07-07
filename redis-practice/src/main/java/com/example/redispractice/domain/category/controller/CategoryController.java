37f16653f5e78005152aa4c650fb56b47af0588c
Sole02 <ljaeseok02@gmail.com>
2026-07-07 12:30:37 +0900

    feat: 카테고리 조회수 집계에 날짜별 Time Window 적용

--- redis-practice/src/main/java/com/example/redispractice/domain/category/controller/CategoryController.java
+++ redis-practice/src/main/java/com/example/redispractice/domain/category/controller/CategoryController.java
@@ -3,10 +3,9 @@ package com.example.redispractice.domain.category.controller;
 import com.example.redispractice.domain.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/category")
@@ -15,12 +14,21 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+//    @GetMapping("/{category}")
+//    public ResponseEntity<String> findCategoryInfo(@PathVariable String category) {
+//        // 카테고리 정보를 조회한 {카테고리}의 뷰 카운트를 올려줄 것
+//        // redis에 저장할 예정
+//        // TV -> /api/category/TV
+//        // Redis에 TV라는 zset 점수를 1 올려줌
+//        return ResponseEntity.ok(categoryService.findCategoryInfo(category));
+//    }
+
+    // Time window 개념을 도입해서 시간, 날짜 별로 구분하여 값을 넣어줄 것
+    // 2025-11-25
+    // 2025-11-26
+    // 2025-11-27 -> 오늘이라고 가정
     @GetMapping("/{category}")
-    public ResponseEntity<String> findCategoryInfo(@PathVariable String category) {
-        // 카테고리 정보를 조회한 {카테고리}의 뷰 카운트를 올려줄 것
-        // redis에 저장할 예정
-        // TV -> /api/category/TV
-        // Redis에 TV라는 zset 점수를 1 올려줌
-        return ResponseEntity.ok(categoryService.findCategoryInfo(category));
+    public ResponseEntity<String> findCategoryInfo(@PathVariable String category, @RequestParam LocalDate currentDate) {
+        return ResponseEntity.ok(categoryService.findCategoryInfo(category, currentDate));
     }
 }
--- redis-practice/src/main/java/com/example/redispractice/domain/category/service/CategoryService.java
+++ redis-practice/src/main/java/com/example/redispractice/domain/category/service/CategoryService.java
@@ -1,20 +1,35 @@
 package com.example.redispractice.domain.category.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
     public static final String CATEGORY_KEY = "category";
+    public static final String CATEGORY_DAILY_KEY = "category:";
     private final StringRedisTemplate stringRedisTemplate;
 
     public String findCategoryInfo(String category) {
         // 외부 URL을 통해 받아온 category를 redis score 1을 올려줌 것
         // redis에 category라는 키 안, 외부에서 받아온 category value에 1점을 올려줄 것
         stringRedisTemplate.opsForZSet().incrementScore(CATEGORY_KEY, category, 1);
+        return category + "을/를 조회 하였습니다.";
+    }
+
+    // Time window 개념을 도입하여 날짜별로 구분하여 값을 넣어줄 것
+    public String findCategoryInfo(String category, LocalDate currentDate) {
+
+        // 2025-11-25 -> category:2025-11-25
+        // 2025-11-26 -> category:2025-11-26
+        // 2025-11-27 -> category:2025-11-27
+        String key = CATEGORY_DAILY_KEY + currentDate.toString();
+        stringRedisTemplate.opsForZSet().incrementScore(key, category, 1);
         return category + "을/를 조회 하였습니다.";
     }
 }
